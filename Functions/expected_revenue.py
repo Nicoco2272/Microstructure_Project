60b418a2ce5f454bd76480424df8e952fc682843
Nicolas Ramirez Castro <ramirezcastronicolas@gmail.com>
2025-02-13 14:47:55 -0600

    Cambios en reporte

--- Functions/expected_revenue.py
+++ Functions/expected_revenue.py
@@ -2,13 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from price_simulation import S_values, pi_LB_S, pi_LS_S, pi_I, p0
 
-# Cálculo de ingresos esperados
-Q = S_values  # Ingresos si todos los traders son de liquidez
-R = (1-pi_I) * S_values # Ingresos para liquidez con la probabilidad dada
+Q = S_values
+R = (1-pi_I) * S_values
 Expected_Revenue = (1-pi_I) * pi_LB_S  * S_values
 
 def plot_bid_ask_spread():
-    """Grafica el spread Bid-Ask y los ingresos esperados."""
     plt.figure(figsize=(8, 5))
     plt.plot(S_values, Q, label="Todos los traders son de liquidez", color="red")
     plt.plot(S_values, R, label="Probabilidad de 40% de un trade informado", color="blue")
